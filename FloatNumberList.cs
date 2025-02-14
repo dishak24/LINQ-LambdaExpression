a7a9cf35591541e75add1543d32bf31561b81f3c
Disha <dishakamble24@gmail.com>
2025-02-14 16:29:11 +0530

    Floating Point List LINQ methods

---
+++ FloatNumberList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaLinq
+{
+    internal class FloatNumberList
+    {
+        List<float> fList = new List<float> 
+        {
+            6.9f,
+            12.8f,
+            5.00f,
+            81.94f,
+            58.9f,
+            77.09f,
+            0.02f
+        };
+
+        public void CalculateAverage()
+        {
+            var avg = fList.Average();
+            Console.WriteLine($"Average of Floating point number List = {avg}");
+        }
+
+    }
+}
