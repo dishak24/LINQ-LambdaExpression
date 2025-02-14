e161d43178d4646d857da0f31192ebac6476379b
Disha <dishakamble24@gmail.com>
2025-02-14 12:27:38 +0530

    Main Method

--- Program.cs
+++ Program.cs
@@ -4,10 +4,26 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-
-
         Console.WriteLine("Welcome to the Lambda-LINQ Expression Practice !");
-        
+        Console.WriteLine();
+
+        NumberLinq numberLinq = new NumberLinq();
+        numberLinq.PrintEven();
+        numberLinq.PrintGreaterNumber();
+        numberLinq.PrintDescending();
+        numberLinq.FindDistinctNumbers();
+        numberLinq.FindMaxNumber();
+        numberLinq.NumberSquare();
+        numberLinq.CalculateOddSum();
+
+
+        StringLinq stringLinq = new StringLinq();
+        stringLinq.StringStartWith();
+        stringLinq.CountString();
+        stringLinq.StringEndWith();
+
+        LinqExample linqExample = new LinqExample();
+        linqExample.PerformWhere();
 
 
     }
